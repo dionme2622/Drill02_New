ade20e45c9dea49f11dbfe5fa7dad997e105dadc
hyunholee <dionme2622@gmail.com>
2023-09-21 12:01:46 +0900

    원점에서 원 운동 완료
    시작점 수정 필요

--- character_grass.py
+++ character_grass.py
@@ -1,5 +1,5 @@
 from pico2d import *
-
+import math
 open_canvas()
 
 character = load_image('character.png')
@@ -13,6 +13,16 @@ delay(1)
 
 def run_circle():
     print('Circle')
+
+    r = 200
+    for deg in range(0, 360, 5):
+        x = r * math.cos(math.radians(deg))
+        y = r * math.sin(math.radians(deg))
+        clear_canvas_now()
+        grass.draw_now(400,30)
+        character.draw_now(x, y)
+        delay(0.1)
+    
     pass
 
 def run_rectangle():
@@ -22,7 +32,7 @@ def run_rectangle():
 while(True) :
     run_circle()
     run_rectangle()
-
+    break
 
     
 
